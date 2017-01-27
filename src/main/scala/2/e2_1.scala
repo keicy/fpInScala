3a417417d746875119d4757e5aaa1857e6230cac
keicy <kei.nakamura97@gmail.com>
2017-01-28 02:05:00 +0900

    2_1, Add another fib

--- src/main/scala/2/e2_1.scala
+++ src/main/scala/2/e2_1.scala
@@ -1,9 +1,17 @@
 object Fib extends App {
-  def fib(n: Int): Int =
+  def fib1(n: Int): Int =
     n match {
       case n if n == 1 => 0
       case n if n == 2 => 1
-      case n => fib(n - 1) + fib(n - 2)
+      case n => fib1(n - 1) + fib1(n - 2)
     }
-  println(fib(10)) // should be 34
+  println(fib1(10)) // should be 34
+
+  def fib2(n: Int): Int = {
+    def loop(n: Int, next: Int, it: Int): Int =
+      if (n == 1) it
+      else loop(n - 1, next + it, next)
+    loop(n, 1, 0) //tailrec
+  }
+  println(fib2(10)) // should be 34
 }
