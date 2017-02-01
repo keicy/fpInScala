04d26f83f85242a2bb5d67511999c3016f076a90
keicy <kei.nakamura97@gmail.com>
2017-02-02 00:47:13 +0900

    Add e6_1

--- src/main/scala/6/e6.scala
+++ src/main/scala/6/e6.scala
@@ -1,5 +1,29 @@
 package e6
 
 trait RNG {
-  def nextInt: (Int, RNG)
+  def nextInt: (Int, RNG) // 引数なし 副作用ないため()省略
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+
+  // e6_1
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    def loop(rng: RNG): (Int, RNG) =
+      rng.nextInt match {
+        case (n, rng) if 0 <= n => (n, rng)
+        case (n, rng) => loop (rng)
+      }
+    loop(rng)
+  }
+
+  def nonNegativeInt2(rng: RNG): (Int, RNG) = {
+    val (i, g) = rng.nextInt
+    (if(i < 0) -(i + 1) else i , g)
+  }
 }
